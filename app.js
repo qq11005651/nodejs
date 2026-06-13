d84686df345e62720d54fda31284708d1b3ee45f
qq11005651 <11005651@qq.com>
2026-06-13 05:13:47 +0000

    feat: init nodejs test app

---
+++ app.js
@@ -0,0 +1,6 @@
+const _ = require('lodash');
+
+console.log("=====================================");
+console.log("Hello, Node.js is running inside Jenkins!");
+console.log("Random number via lodash:", _.random(1, 100));
+console.log("=====================================");
